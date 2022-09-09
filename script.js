fd3def854136437cb0bc7230478b66a95bdbd199
Strongie <nickstrong665@gmail.com>
2022-09-09 23:54:10 +1000

    add url fetch function

--- script.js
+++ script.js
@@ -4,15 +4,35 @@
 var searchButton = document.querySelector("searchbutton")
 
 //event when the submit button is pressed
-
 //fetch the api data
+
+var repoList = document.querySelector('ul');
+var searchButton = document.getElementById('searchbutton');
+
+function getApi() {
+  // replace `octocat` with anyone else's GitHub username
+  var requestUrl = 'https://api.github.com/users/octocat/repos';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+searchButton.addEventListener('click', getApi);
 
 // save city search to new button
 
 //city button becomes visible when text inserted
 
 //save the search city to local storage
-
 //var saveButtonEL1 = document.querySelector('#saveBtn1');
  function storeActivity1() {
     var cityname = document.getElementById("cityname").value;
@@ -24,7 +44,6 @@ var searchButton = document.querySelector("searchbutton")
  }
 
 //retreive city from local storage
-
 
 function reload() {
     var x = localStorage.getItem("cityname");
